078518e49bbe20537870c0fa8f0483463de25e1a
kottragu <alexklim0120@gmail.com>
2021-03-28 15:48:43 +0300

    add Service

---
+++ src/main/java/project/vaadin/services/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package project.vaadin.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.User.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import project.vaadin.entity.User;
+import project.vaadin.repo.UserRepo;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        UserBuilder userBuilder = org.springframework.security.core.userdetails.User.withUsername(username);
+        userBuilder.password(new BCryptPasswordEncoder().encode(user.getPassword()));
+        userBuilder.roles(user.getRole().toString());
+        return userBuilder.build();
+    }
+
+}
